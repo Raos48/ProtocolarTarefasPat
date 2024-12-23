f7f7066c8d7c780013d13b648aa37f3890d6a52d
Raos48 <131822770+Raos48@users.noreply.github.com>
2024-12-23 11:55:22 -0300

    Atualizar código

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'Sua_chave_pública_aqui'
+    APP_NAME = 'ProtocoloTarefasPAT'
+    COMPANY_NAME = 'ProtocoloTarefasPAT'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['https://raw.githubusercontent.com/Raos48/ProtocolarTarefasPat/main/updates']
